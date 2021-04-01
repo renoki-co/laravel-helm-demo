3e3cec41cbf1c4c922a7bcc3d4e027ad02330b4f
Alex Renoki <rennokki@gmail.com>
2021-04-02 00:11:13 +0300

    Added $_ENV for pod name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -124,6 +124,11 @@
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+
+                        @if ($_ENV['POD_NAME'] ?? false)
+                            <br>
+                            Pod name: {{ $_ENV['POD_NAME'] }}
+                        @endif
                     </div>
                 </div>
             </div>
