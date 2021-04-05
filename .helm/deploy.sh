583e52e522b7c3176e0495ee0c8203a091aa4756
Alex Renoki <rennokki@gmail.com>
2021-04-05 13:10:08 +0300

    Updated chart version

--- .helm/deploy.sh
+++ .helm/deploy.sh
@@ -2,20 +2,39 @@
 
 helm repo add renoki-co https://helm.renoki.org
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add kube-state-metrics https://kubernetes.github.io/kube-state-metrics
 helm repo update
 
 # Deploy the Secret that will contain the .env file.
 kubectl apply -f secret.yaml
 
-# Optional: deploy NGINX Ingress Controller into your cluster.
+# Optional: deploy NGINX Ingress Controller into your cluster
+# to expose the ingress outside the cluster.
 helm upgrade nginx \
     --version=3.26.0 \
     -f nginx-values.yaml \
     --install \
     ingress-nginx/ingress-nginx
 
+# Optional: deploy Prometheus & Prometheus Adapter to scrape
+# the pod metrics (if PHP-FPM and NGINX exporters are enabled)
+# to automatically scale the containers based on the pm.max_children value.
+helm upgrade prometheus \
+    --version=13.6.0 \
+    -f prometheus-values.yaml \
+    --install \
+    prometheus-community/prometheus
+
+helm upgrade prometheus-adapter \
+    --version=2.12.1 \
+    -f prometheus-adapter-values.yaml \
+    --install \
+    prometheus-community/prometheus-adapter
+
+# Deploy the Laravel app.
 helm upgrade laravel \
-    --version=0.4.2 \
+    --version=0.6.0 \
     -f laravel-values.yaml \
     --install \
     renoki-co/laravel
--- README.md
+++ README.md
@@ -5,6 +5,7 @@
     - [Change PHP-FPM version](#change-php-fpm-version)
   - [Deploying](#deploying)
     - [Helm v3](#helm-v3)
+    - [Autoscaling](#autoscaling)
 
 # Laravel Helm Demo
 
@@ -46,3 +47,26 @@ $ cd .helm && sh deploy.sh
 ```
 
 The example also comes with NGINX Ingress Controller and is going to be deployed to the cluster.
+
+### Autoscaling
+
+For better understading of autoscaling, Prometheus and Prometheus Adapter may be used to scrap the PHP-FPM Process Manager's active children and scale pods up or down based on the number.
+
+There is an article that explains the way this works: [Scaling PHP FPM based on utilization demand on Kubernetes](https://blog.wyrihaximus.net/2021/01/scaling-php-fpm-based-on-utilization-demand-on-kubernetes/).
+
+The only setting you should be aware of is that there are two containers in the Laravel pod that expose metrics. To allow Prometheus to scrape them both, don't use the port annotation on the pod and add the following source label in the Prometheus job ([original gist](https://gist.github.com/bakins/5bf7d4e719f36c1c555d81134d8887eb)):
+
+```yaml
+jobs:
+  - job_name: 'kubernetes-pods'
+    ...
+
+    relabel_configs:
+      ...
+
+      - source_labels: [__meta_kubernetes_pod_container_port_name]
+        action: keep
+        regex: (.+)-metrics
+
+      ...
+```
